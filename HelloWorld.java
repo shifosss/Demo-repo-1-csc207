a86916832d6965c7af953afcaa3802dea0034535
shifosss <93196351+shifosss@users.noreply.github.com>
2024-09-05 17:16:39 -0400

    Initial stage

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("I'm glad to be here.");
+    }
+}
